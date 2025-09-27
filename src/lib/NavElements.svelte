2bb65e4835593d17f3f3bc1c33a2fe399845ec37
booukes <lukaszandrzejpilat@gmail.com>
2025-09-27 14:46:28 +0200

    added persistent scale on navbar elements

--- src/lib/NavElements.svelte
+++ src/lib/NavElements.svelte
@@ -1,22 +1,31 @@
-<script>
+<script lang="ts">
+	import { page } from '$app/stores';
 </script>
 
 <nav class="flex flex-row items-end">
 	<a
 		href="/about"
 		class="m-4 transform font-serif text-2xl font-normal transition-transform duration-100
-               hover:scale-110">about</a
+			hover:scale-110 {$page.url.pathname === '/about' ? 'scale-110 font-bold text-blue-400' : ''}"
 	>
+		about
+	</a>
 	<div class="m-4 h-6 border-l"></div>
+
 	<a
 		href="/projects"
 		class="m-4 transform font-serif text-2xl font-normal transition-transform duration-100
-               hover:scale-110">projects</a
+			hover:scale-110 {$page.url.pathname === '/projects' ? 'scale-110 font-bold text-blue-400' : ''}"
 	>
+		projects
+	</a>
 	<div class="m-4 h-6 border-l"></div>
+
 	<a
 		href="/contact"
 		class="m-4 transform font-serif text-2xl font-normal transition-transform duration-100
-               hover:scale-110">contact</a
+			hover:scale-110 {$page.url.pathname === '/contact' ? 'scale-110 font-bold text-blue-400' : ''}"
 	>
+		contact
+	</a>
 </nav>
